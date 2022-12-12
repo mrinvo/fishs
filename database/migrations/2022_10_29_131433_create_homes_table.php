1161ce8315e37a38f74633711800c4ae9b43088d
mrinvo <huss.mostafa12@gmail.com>
2022-12-12 15:21:18 +0200

    jlsdkfj

---
+++ database/migrations/2022_10_29_131433_create_homes_table.php
@@ -0,0 +1,35 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('homes', function (Blueprint $table) {
+            $table->id();
+            $table->text('text_en')->nullable();
+            $table->text('text_ar')->nullable();
+            $table->text('img')->nullable();
+            $table->string('wa_phone');
+            $table->boolean('mode')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('homes');
+    }
+};
